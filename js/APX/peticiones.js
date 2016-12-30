6cda56c09ce06e765494a637185b5af7ade9151a
xe27219 <gerardo.martin.contractor@bbva.com>
2016-12-30 10:02:00 +0100

    Fecha APX

--- js/APX/peticiones.js
+++ js/APX/peticiones.js
@@ -78,7 +78,7 @@ $(document).ready(function() {
             type: 'line',
             data:[]
           },{
-            name: 'Max. Peticiones (05/12)',
+            name: 'Max. Peticiones (13/12)',
             color: 'rgba(255,0,0,1.0)',
             type: 'line',
             data:[]
