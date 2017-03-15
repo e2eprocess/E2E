f2de95f11d744bdaa0c7911659a52a6facac472c
Gero <gerardo.martin.contractor@bbva.com>
2017-03-15 10:06:08 +0100

    Informe Semanal Cash 15/03/2017

--- js/netCash/KYOP/aplicacion_mensual.js
+++ js/netCash/KYOP/aplicacion_mensual.js
@@ -117,7 +117,6 @@ $(document).ready(function() {
               color: '#333333',
               fillColor: 'rgba(255,255,255,0.8)',
               data: [
-                  { x: 1486026000000, text: '<b>Pico historico tráfico real</b> <br/>Se ha alcanzado el máximo de peticiones (561.829 por hora)', title: 'Pico historico tráfico real' },
                   { x: 1487422800000, text: '<b>Pruebas semestrales</b> <br/>Pruebas de rendimiento semestrales sobre el CASH', title: 'Pruebas semestrales' },
                   { x: 1488841200000, text: '<b>Pruebas LDAP</b> <br/>Pruebas incidencia operativas LDAP', title: 'Pruebas LDAP' },
                   { x: 1488708000000, text: '<b>Pruebas Post implantación</b> <br/>Pruebas Cash post implantación', title: 'Pruebas Post implantación' }
